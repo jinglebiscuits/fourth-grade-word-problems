e880422a44836b72eaade8c2ecde081a0b7e05a6
jinglebiscuits <scott.wehby@gmail.com>
2021-04-04 14:51:50 -0500

    added student model without Garden

---
+++ app/src/main/java/com/completewordproblems/fourthgrade/models/Student.kt
@@ -0,0 +1,9 @@
+package com.completewordproblems.fourthgrade.models
+
+data class Student(
+    var name: String,
+    var age: Int,
+    var grade: Int,
+    val activeConcepts: List<Concept>,
+    val masteredConcepts: List<Concept>
+)
