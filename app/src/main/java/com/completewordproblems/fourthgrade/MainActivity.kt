e1946efa9bebb76e982955f896cea23fda63aef5
jinglebiscuits <scott.wehby@gmail.com>
2021-04-04 20:20:02 -0500

    don't use appcompatbuttons

--- app/src/main/java/com/completewordproblems/fourthgrade/MainActivity.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/MainActivity.kt
@@ -2,12 +2,6 @@ package com.completewordproblems.fourthgrade
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
