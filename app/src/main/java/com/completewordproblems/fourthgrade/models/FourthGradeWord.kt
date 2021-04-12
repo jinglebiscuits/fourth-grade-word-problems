1b46ca950a6a15726157194163f9cba5ce264c3a
jinglebiscuits <scott.wehby@gmail.com>
2021-04-11 22:11:00 -0500

    following IDE suggestion for data class

--- app/src/main/java/com/completewordproblems/fourthgrade/models/FourthGradeWord.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/models/FourthGradeWord.kt
@@ -4,4 +4,24 @@ data class FourthGradeWord(
     val word: String,
     val correctDefinition: String,
     val incorrectDefinitions: Array<String>
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FourthGradeWord
+
+        if (word != other.word) return false
+        if (correctDefinition != other.correctDefinition) return false
+        if (!incorrectDefinitions.contentEquals(other.incorrectDefinitions)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = word.hashCode()
+        result = 31 * result + correctDefinition.hashCode()
+        result = 31 * result + incorrectDefinitions.contentHashCode()
+        return result
+    }
+}
