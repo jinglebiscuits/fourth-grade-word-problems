166afe666f275a9e10b54872db798438b443b491
jinglebiscuits <scott.wehby@gmail.com>
2021-04-11 22:44:46 -0500

    you have to pick the correct definition to get credit for the key word

--- app/src/main/java/com/completewordproblems/fourthgrade/fragments/VocabularyDialogFragment.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/fragments/VocabularyDialogFragment.kt
@@ -2,6 +2,7 @@ package com.completewordproblems.fourthgrade.fragments
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
@@ -13,14 +14,17 @@ import com.completewordproblems.fourthgrade.models.KeyWord
 class VocabularyDialogFragment(
     private val vocabularyDialogListener: VocabularyDialogListener,
     private val keyWord: KeyWord
-) :
-    DialogFragment() {
+) : DialogFragment() {
+
+    private var correctPosition = 0
+    private var chosenPosition = -1
+    private lateinit var dialog: AlertDialog
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val customView = layoutInflater.inflate(R.layout.dialog_vocabulary_quiz, null)
         val definitionOptions: ListView =
             customView.findViewById(R.id.definition_options)
-        var options: MutableList<String> = arrayListOf()
+        val options: MutableList<String> = arrayListOf()
         FourthGradeDictionary.getWord(keyWord.keyWord)
             ?.let { options.addAll(it.incorrectDefinitions) }
         FourthGradeDictionary.getWord(keyWord.keyWord)?.let { options.add(it.correctDefinition) }
@@ -28,18 +32,34 @@ class VocabularyDialogFragment(
             requireContext(), android.R.layout.simple_list_item_single_choice,
             options
         )
+        correctPosition =
+            options.indexOf(FourthGradeDictionary.getWord(keyWord.keyWord)?.correctDefinition)
         definitionOptions.adapter = adapter
-        return AlertDialog.Builder(requireContext())
+        definitionOptions.setOnItemClickListener { parent, view, position, id ->
+            Log.d("VocabDialogFrag", "position is $position")
+            chosenPosition = position
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = true
+        }
+        dialog = AlertDialog.Builder(requireContext())
             .setTitle("Select the correct definition for: ${keyWord.keyWord}")
             .setView(customView)
             .setPositiveButton(
                 "Submit"
-            ) { _, _ -> vocabularyDialogListener.onSubmit(keyWord) }
+            ) { _, _ ->
+                vocabularyDialogListener.onSubmit(
+                    keyWord,
+                    correctPosition == chosenPosition
+                )
+            }
             .create()
+        dialog.setOnShowListener { dialog ->
+            (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+        }
+        return dialog
     }
 
     interface VocabularyDialogListener {
-        fun onSubmit(keyWord: KeyWord)
+        fun onSubmit(keyWord: KeyWord, isCorrect: Boolean)
     }
 
     companion object {
