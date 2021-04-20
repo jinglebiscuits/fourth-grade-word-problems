bbb12b5fb6138cb3a07a07bf7f20c620c71c2f75
jinglebiscuits <scott.wehby@gmail.com>
2021-04-19 20:44:19 -0500

    implemented free response UI for what are you looking for frag

--- app/src/main/java/com/completewordproblems/fourthgrade/fragments/WhatAreYouLookingForFragment.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/fragments/WhatAreYouLookingForFragment.kt
@@ -1,11 +1,14 @@
 package com.completewordproblems.fourthgrade.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.completewordproblems.fourthgrade.R
+import com.completewordproblems.fourthgrade.Wizard
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +25,8 @@ class WhatAreYouLookingForFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var wordProblemTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,9 +38,20 @@ class WhatAreYouLookingForFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_what_are_you_looking_for, container, false)
+    ): View {
+        val view: View =
+            inflater.inflate(R.layout.fragment_what_are_you_looking_for, container, false)
+        val text: TextView = view.findViewById(R.id.word_problem_text)
+        text.text = Wizard.currentProblem.getWordProblemText()
+        view.findViewById<View>(R.id.next_button).setOnClickListener(View.OnClickListener {
+            // TODO: 4/4/21 This navigation should be based on the Student's strategy algorithm
+            view.findNavController()
+                .navigate(R.id.action_practiceFragment_to_whatIsGivenFragment)
+        })
+        view.findViewById<View>(R.id.back_button).setOnClickListener {
+            view.findNavController().navigate(R.id.practiceFragment)
+        }
+        return view
     }
 
     companion object {
