ae5ddafba03454d84dfc2989efda92dc453dea10
jinglebiscuits <scott.wehby@gmail.com>
2021-04-22 20:37:09 -0500

    add path to draw fragment

--- app/src/main/java/com/completewordproblems/fourthgrade/fragments/RemoveInformationFragment.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/fragments/RemoveInformationFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.findNavController
 import com.completewordproblems.fourthgrade.R
+import com.completewordproblems.fourthgrade.Wizard
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +25,8 @@ class RemoveInformationFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var wordProblemTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,9 +38,20 @@ class RemoveInformationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_remove_information, container, false)
+    ): View {
+        val view: View = inflater.inflate(R.layout.fragment_draw_scene, container, false)
+        wordProblemTextView = view.findViewById(R.id.word_problem_text)
+        wordProblemTextView.text = Wizard.currentProblem.getWordProblemText()
+        view.findViewById<View>(R.id.next_button).setOnClickListener(View.OnClickListener {
+            // TODO: 4/4/21 This navigation should be based on the Student's strategy algorithm
+            view.findNavController()
+                .navigate(R.id.action_practiceFragment_to_drawSceneFragment)
+        })
+        view.findViewById<View>(R.id.back_button).setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_practiceFragment_to_whatIsGivenFragment)
+        }
+        return view
     }
 
     companion object {
