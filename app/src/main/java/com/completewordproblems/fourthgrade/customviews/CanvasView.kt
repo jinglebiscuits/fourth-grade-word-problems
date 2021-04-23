e65413d0b9956bbe5dd94786ad7de17f49fb047e
jinglebiscuits <scott.wehby@gmail.com>
2021-04-23 08:33:26 -0500

    starting a class for drawing

---
+++ app/src/main/java/com/completewordproblems/fourthgrade/customviews/CanvasView.kt
@@ -0,0 +1,12 @@
+package com.completewordproblems.fourthgrade.customviews
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.View
+
+class CanvasView(context: Context?) : View(context) {
+    var path = Path()
+    var paint = Paint()
+
+}
