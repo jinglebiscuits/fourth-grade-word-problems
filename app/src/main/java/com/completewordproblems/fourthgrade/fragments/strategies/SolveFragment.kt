d5981c8831cc0265ff7a8e5b5990c10e3ab3f8b7
jinglebiscuits <scott.wehby@gmail.com>
2021-05-02 09:10:11 -0500

    solution is checked

--- app/src/main/java/com/completewordproblems/fourthgrade/fragments/strategies/SolveFragment.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/fragments/strategies/SolveFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.completewordproblems.fourthgrade.R
+import com.completewordproblems.fourthgrade.Wizard
 
 /**
  * A simple [Fragment] subclass.
@@ -14,6 +18,9 @@ import com.completewordproblems.fourthgrade.R
  */
 class SolveFragment : StrategyFragmentBase("Solve the problem") {
 
+    lateinit var answerText: EditText
+    lateinit var submitButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +28,18 @@ class SolveFragment : StrategyFragmentBase("Solve the problem") {
         val view: View = inflater.inflate(R.layout.fragment_solve, container, false)
         setupWordProblemText(view)
         setupNavigation(view)
+        answerText = view.findViewById(R.id.answer_text)
+        submitButton = view.findViewById(R.id.submit_button)
+        submitButton.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            if (Wizard.currentProblem.answer == answerText.text.toString()) {
+                builder.setMessage("That's right!")
+            } else {
+                builder.setMessage("${answerText.text} isn't the correct answer. Please try again.")
+            }
+            builder.setPositiveButton(android.R.string.ok, null)
+            builder.create().show()
+        }
         return view
     }
 }
