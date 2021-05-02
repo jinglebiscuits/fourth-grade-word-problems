0d4b579b82bf61906a442ed394cd11b8dab5e5f9
jinglebiscuits <scott.wehby@gmail.com>
2021-05-02 11:19:30 -0500

    added key word to xml interpreter

--- app/src/main/java/com/completewordproblems/fourthgrade/WordProblemXmlParser.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/WordProblemXmlParser.kt
@@ -1,6 +1,8 @@
 package com.completewordproblems.fourthgrade
 
 import android.util.Xml
+import com.completewordproblems.fourthgrade.models.Concept
+import com.completewordproblems.fourthgrade.models.KeyWord
 import com.completewordproblems.fourthgrade.models.WordProblem
 import com.completewordproblems.fourthgrade.models.WordProblemSegment
 import org.xmlpull.v1.XmlPullParser
@@ -86,6 +88,7 @@ public class WordProblemXmlParser {
         var segment = ""
         var isNecessary = false
         var isMainObjective = false
+        var keyWords = arrayListOf<KeyWord>()
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.eventType != XmlPullParser.START_TAG) {
                 continue
@@ -94,10 +97,73 @@ public class WordProblemXmlParser {
                 "text" -> segment = readSegmentText(parser)
                 "is_necessary" -> isNecessary = readIsNecessary(parser)
                 "is_main_objective" -> isMainObjective = readIsMainObjective(parser)
+                "key_words" -> keyWords = readKeyWords(parser)
                 else -> skip(parser)
             }
         }
-        return WordProblemSegment(segment, isNecessary, isMainObjective)
+        val wpSegment = WordProblemSegment(segment, isNecessary, isMainObjective)
+        keyWords.forEach { wpSegment.addKeyword(it) }
+        return wpSegment
+    }
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun readKeyWords(parser: XmlPullParser): ArrayList<KeyWord> {
+        val keyWords = arrayListOf<KeyWord>()
+        parser.require(XmlPullParser.START_TAG, ns, "key_words")
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.eventType != XmlPullParser.START_TAG) {
+                continue
+            }
+            when (parser.name) {
+                "key_word" -> keyWords.add(readKeyWord(parser))
+                else -> skip(parser)
+            }
+        }
+        return keyWords
+    }
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun readKeyWord(parser: XmlPullParser): KeyWord {
+        parser.require(XmlPullParser.START_TAG, ns, "key_word")
+        var start = 0
+        var end = 0
+        var word = ""
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.eventType != XmlPullParser.START_TAG) {
+                continue
+            }
+            when (parser.name) {
+                "start" -> start = readKeyWordStart(parser)
+                "end" -> end = readKeyWordEnd(parser)
+                "word" -> word = readKeyWordWord(parser)
+                else -> skip(parser)
+            }
+        }
+        return KeyWord(start, end, word, Concept(listOf(), 0, 0))
+    }
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun readKeyWordStart(parser: XmlPullParser): Int {
+        parser.require(XmlPullParser.START_TAG, ns, "start")
+        val start = readInt(parser)
+        parser.require(XmlPullParser.END_TAG, ns, "start")
+        return start
+    }
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun readKeyWordEnd(parser: XmlPullParser): Int {
+        parser.require(XmlPullParser.START_TAG, ns, "end")
+        val end = readInt(parser)
+        parser.require(XmlPullParser.END_TAG, ns, "end")
+        return end
+    }
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun readKeyWordWord(parser: XmlPullParser): String {
+        parser.require(XmlPullParser.START_TAG, ns, "word")
+        val word = readText(parser)
+        parser.require(XmlPullParser.END_TAG, ns, "word")
+        return word
     }
 
     @Throws(XmlPullParserException::class, IOException::class)
@@ -150,6 +216,16 @@ public class WordProblemXmlParser {
             parser.nextTag()
         }
         return result == "true"
+    }
+
+    @Throws(IOException::class, XmlPullParserException::class)
+    private fun readInt(parser: XmlPullParser): Int {
+        var result = 0
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.text.toInt()
+            parser.nextTag()
+        }
+        return result
     }
 
     @Throws(XmlPullParserException::class, IOException::class)
--- app/src/main/java/com/completewordproblems/fourthgrade/models/FourthGradeDictionary.kt
+++ app/src/main/java/com/completewordproblems/fourthgrade/models/FourthGradeDictionary.kt
@@ -7,6 +7,10 @@ object FourthGradeDictionary {
             "recent",
             "during the period of time that has just passed : not long ago",
             arrayOf("of relatively late occurrence or appearance", "a long time ago")
+        ), "simplest form" to FourthGradeWord(
+            "simplest form",
+            "1 is the only common factor of the numerator and denominator",
+            arrayOf("The numerator is divisible by the denominator", "The denominator is divisible by the numerator")
         )
     )
 
